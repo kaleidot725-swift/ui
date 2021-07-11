0490f4cec6cc7e08ce6281a902b8f07c65feffcc
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-07-11 16:10:41 +0900

    feat: add Creating and Combining Views

---
+++ Creating and Combining Views/Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by kaleidot725 on 2021/07/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes her e.")
+            }.padding()
+            
+            Spacer()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
